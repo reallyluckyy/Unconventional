74fd8ac862899a3c7e80047085d3c589755b6eed
Luca Berndt <luca.berndt@gmail.com>
2022-06-01 10:30:05 +0200

    Add hooking support

---
+++ src/HookingTests.cpp
@@ -0,0 +1,254 @@
+#include <cassert>
+
+#include "Unconventional.hpp"
+
+// We have to use naked functions here as we need to be sure what this compiles to,
+// as we need to specify the opcode size later for hooking
+
+void __declspec(naked) Subtract_ArgumentsStackOnly(/*int32_t a, int32_t b*/)
+{
+	__asm
+	{
+		mov eax, [esp + 4]
+		sub eax, [esp + 8]
+		ret
+	}
+}
+
+void __declspec(naked) Subtract_ArgumentsRegistersOnly(/*int32_t<eax> a, int32_t<ebx> b*/)
+{
+	__asm
+	{
+		sub eax, ebx
+		nop
+		nop
+		nop
+		ret
+	}
+}
+
+void __declspec(naked) Subtract_ArgumentsMixed(/*int32_t<eax> a, int32_t b*/)
+{
+	__asm
+	{
+		sub eax, [esp + 4]
+		nop
+		ret
+	}
+}
+
+
+
+namespace BasicRedirectionTests
+{
+
+	int32_t Subtract_Hook(int32_t a, int32_t b)
+	{
+		return b - a;
+	}
+
+	void Run()
+	{
+		using namespace Unconventional;
+
+		int eaxValue = 0;
+
+		constexpr int parameter1 = 10;
+		constexpr int parameter2 = 8;
+		int expectedHookResult = Subtract_Hook(parameter1, parameter2);
+		int expectedOriginalResult = parameter1 - parameter2;
+
+		{
+			Function<int32_t, int32_t, int32_t> function((uintptr_t)&Subtract_ArgumentsStackOnly, {});
+			Hook hook(function, (uintptr_t)&Subtract_Hook, 8);
+			hook.Install();
+			
+			__asm
+			{
+				push parameter2
+				push parameter1
+				call Subtract_ArgumentsStackOnly
+				add esp, 8
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedHookResult);
+			
+			hook.Uninstall();
+
+			__asm
+			{
+				push parameter2
+				push parameter1
+				call Subtract_ArgumentsStackOnly
+				add esp, 8
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedOriginalResult);
+		}
+
+		{
+			Function<int32_t, int32_t, int32_t> function((uintptr_t)&Subtract_ArgumentsRegistersOnly, { {Location::EAX, Location::EBX} });
+			Hook hook(function, (uintptr_t)&Subtract_Hook, 5);
+			hook.Install();
+
+			__asm
+			{
+				mov ebx, parameter2
+				mov eax, parameter1
+				call Subtract_ArgumentsRegistersOnly
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedHookResult);
+
+			hook.Uninstall();
+
+			__asm
+			{
+				mov ebx, parameter2
+				mov eax, parameter1
+				call Subtract_ArgumentsRegistersOnly
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedOriginalResult);
+		}
+
+		{
+			Function<int32_t, int32_t, int32_t> function((uintptr_t)&Subtract_ArgumentsMixed, { {Location::EAX, Location::Stack} });
+			Hook hook(function, (uintptr_t)&Subtract_Hook, 5);
+			hook.Install();
+
+			__asm
+			{
+				push parameter2
+				mov eax, parameter1
+				call Subtract_ArgumentsMixed
+				add esp, 4
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedHookResult);
+
+			hook.Uninstall();
+
+			__asm
+			{
+				push parameter2
+				mov eax, parameter1
+				call Subtract_ArgumentsMixed
+				add esp, 4
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedOriginalResult);
+		}
+	}
+}
+
+namespace TrampolineTests
+{
+	Unconventional::Hook<int32_t, int32_t, int32_t> hook;
+
+	int32_t Subtract_Hook(int32_t a, int32_t b)
+	{
+		return hook.CallOriginalFunction(a, b) + 100;
+	}
+
+	void Run()
+	{
+		using namespace Unconventional;
+
+		int eaxValue = 0;
+
+		constexpr int parameter1 = 10;
+		constexpr int parameter2 = 8;
+		constexpr int expectedOriginalResult = parameter1 - parameter2;
+		constexpr int expectedHookResult = expectedOriginalResult + 100;
+
+		{
+			Function<int32_t, int32_t, int32_t> function((uintptr_t)&Subtract_ArgumentsStackOnly, {});
+			hook = Hook(function, (uintptr_t)&Subtract_Hook, 8);
+			hook.Install();
+			
+			__asm
+			{
+				push parameter2
+				push parameter1
+				call Subtract_ArgumentsStackOnly
+				add esp, 8
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedHookResult);
+			
+			hook.Uninstall();
+
+			__asm
+			{
+				push parameter2
+				push parameter1
+				call Subtract_ArgumentsStackOnly
+				add esp, 8
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedOriginalResult);
+		}
+
+		{
+			Function<int32_t, int32_t, int32_t> function((uintptr_t)&Subtract_ArgumentsRegistersOnly, { {Location::EAX, Location::EBX} });
+			hook = Hook(function, (uintptr_t)&Subtract_Hook, 5);
+			hook.Install();
+
+			__asm
+			{
+				mov ebx, parameter2
+				mov eax, parameter1
+				call Subtract_ArgumentsRegistersOnly
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedHookResult);
+
+			hook.Uninstall();
+
+			__asm
+			{
+				mov ebx, parameter2
+				mov eax, parameter1
+				call Subtract_ArgumentsRegistersOnly
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedOriginalResult);
+		}
+
+		{
+			Function<int32_t, int32_t, int32_t> function((uintptr_t)&Subtract_ArgumentsMixed, { {Location::EAX, Location::Stack} });
+			hook = Hook(function, (uintptr_t)&Subtract_Hook, 5);
+			hook.Install();
+
+			__asm
+			{
+				push parameter2
+				mov eax, parameter1
+				call Subtract_ArgumentsMixed
+				add esp, 4
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedHookResult);
+
+			hook.Uninstall();
+
+			__asm
+			{
+				push parameter2
+				mov eax, parameter1
+				call Subtract_ArgumentsMixed
+				add esp, 4
+				mov eaxValue, eax
+			}
+			assert(eaxValue == expectedOriginalResult);
+		}
+	}
+}
+
+
+void RunHookingTests()
+{
+	BasicRedirectionTests::Run();
+	TrampolineTests::Run();
+}
--- src/Unconventional.hpp
+++ src/Unconventional.hpp
@@ -1,24 +1,21 @@
 #pragma once
 
 #include <stdexcept>
-#include <algorithm>
+#include <functional>
 #include <cstdint>
 #include <optional>
 #include <array>
 #include <vector>
 #include <any>
 
+#include <Windows.h>
+
+
 namespace Unconventional
 {
-	enum class CallingConvention
-	{
-		CDECL,
-		// TODO: Support more calling conventions
-	};
-
 	enum class Location
 	{
-		STACK,
+		Stack,
 		EAX, EBX, ECX, EDX, ESI, EDI,
 		AH, AL, BH, BL, CH, CL, DH, DL, SIL, DIL,
 		ST0, ST1, ST2, ST3, ST4, ST5, ST6, ST7
@@ -27,50 +24,108 @@ namespace Unconventional
 		// TODO: Support XMM registers
 	};
 
+	class CallingConvention
+	{
+	public:
+		enum Convention
+		{
+			Cdecl
+		};
+
+		CallingConvention(Convention value = Cdecl) : value(value)
+		{
+		}
+
+		Convention GetValue() const { return value; }
+
+		Location GetReturnValueLocation(const bool isFloatingPointValue) const
+		{
+			switch (value)
+			{
+			case Cdecl:
+				if (isFloatingPointValue)
+					return Location::ST0;
+				else
+					return Location::EAX;
+			default:
+				throw std::exception("Not implemented");
+			}
+		}
+
+		bool SpecifiesCallerCleanup() const
+		{
+			switch (value)
+			{
+			case Cdecl:
+				return true;
+			default:
+				throw std::exception("Not implemented");
+			}
+
+			return false;
+		}
+
+	private:
+
+		Convention value;
+	};
+
+	namespace Utils
+	{
+		static uint8_t GetLowByte(uint32_t x)
+		{
+			return x & 0xFF;
+		}
+
+		static uint8_t GetHighByte(uint32_t x)
+		{
+			return (x >> 8) & 0xFF;
+		}
+	}
+
 	template<typename ReturnType, typename... ArgumentTypes>
 	class FunctionSignature
 	{
 	public:
-		FunctionSignature(const CallingConvention callingConvention = CallingConvention::CDECL) :
+		FunctionSignature(const CallingConvention callingConvention = CallingConvention::Cdecl) :
 			callingConvention(callingConvention), returnValueLocation(), argumentLocations(std::array<Location, sizeof...(ArgumentTypes)>())
 		{
 		}
 
-		FunctionSignature(Location returnValueLocation, const CallingConvention callingConvention = CallingConvention::CDECL)
+		FunctionSignature(Location returnValueLocation, const CallingConvention callingConvention = CallingConvention::Cdecl)
 			: callingConvention(callingConvention), returnValueLocation(returnValueLocation), argumentLocations(std::array<Location, sizeof...(ArgumentTypes)>())
 		{
 		}
 
-		FunctionSignature(Location returnValueLocation, std::array<Location, sizeof...(ArgumentTypes)> argumentLocations, const CallingConvention callingConvention = CallingConvention::CDECL)
+		FunctionSignature(Location returnValueLocation, std::array<Location, sizeof...(ArgumentTypes)> argumentLocations, const CallingConvention callingConvention = CallingConvention::Cdecl)
 			: callingConvention(callingConvention), returnValueLocation(returnValueLocation), argumentLocations(argumentLocations)
 		{
 			VerifyReturnValueLocation();
 			VerifyArgumentLocations();
 		}
 
-		FunctionSignature(std::array<Location, sizeof...(ArgumentTypes)> argumentLocations, const CallingConvention callingConvention = CallingConvention::CDECL)
+		FunctionSignature(std::array<Location, sizeof...(ArgumentTypes)> argumentLocations, const CallingConvention callingConvention = CallingConvention::Cdecl)
 			: callingConvention(callingConvention), returnValueLocation(), argumentLocations(argumentLocations)
 		{
 			VerifyReturnValueLocation();
 			VerifyArgumentLocations();
 		}
 
+		CallingConvention GetCallingConvention() const
+		{
+			return callingConvention;
+		}
+
 		Location GetReturnValueLocation() const
 		{
 			if (returnValueLocation.has_value())
 				return returnValueLocation.value();
-			return GetCallingConventionReturnValueLocation();
+			return callingConvention.GetReturnValueLocation(std::is_floating_point<ReturnType>());
 		}
 
-		bool ConventionSpecifiesCallerCleanup() const
+		std::array<Location, sizeof...(ArgumentTypes)> GetArgumentLocations()
 		{
-			switch(callingConvention)
-			{
-			case CallingConvention::CDECL:
-				return true;
-			}
-
-			return false;
+			return argumentLocations;
 		}
 
 		std::vector<int32_t> GetStackArgumentIndices()
@@ -78,7 +133,7 @@ namespace Unconventional
 			std::vector<int32_t> indices;
 			for (int32_t i = 0; i < (int32_t)sizeof...(ArgumentTypes); i++)
 			{
-				if (argumentLocations[i] == Location::STACK)
+				if (argumentLocations[i] == Location::Stack)
 				{
 					indices.push_back(i);
 				}
@@ -88,8 +143,8 @@ namespace Unconventional
 
 		int32_t GetArgumentIndexForRegister(Location location)
 		{
-			if (location == Location::STACK)
-				throw std::invalid_argument("Location passed to GetArgumentIndexForRegister was Location::STACK");
+			if (location == Location::Stack)
+				throw std::invalid_argument("Location passed to GetArgumentIndexForRegister was Location::Stack");
 
 			for (int32_t i = 0; i < (int32_t)sizeof...(ArgumentTypes); i++)
 			{
@@ -104,8 +159,8 @@ namespace Unconventional
 
 		bool HasArgumentInRegister(Location location)
 		{
-			if (location == Location::STACK)
-				throw std::invalid_argument("Location passed to HasArgumentInRegister was Location::STACK");
+			if (location == Location::Stack)
+				throw std::invalid_argument("Location passed to HasArgumentInRegister was Location::Stack");
 
 			return this->GetArgumentIndexForRegister(location) != -1;
 		}
@@ -115,25 +170,10 @@ namespace Unconventional
 		std::optional<Location> returnValueLocation;
 		std::array<Location, sizeof...(ArgumentTypes)> argumentLocations;
 
-		Location GetCallingConventionReturnValueLocation() const
-		{
-			switch (callingConvention)
-			{
-			case CallingConvention::CDECL:
-				if (std::is_floating_point<ReturnType>())
-					return Location::ST0;
-				else
-					return Location::EAX;
-			default:
-				throw std::exception("Not implemented");
-			}
-		}
-
 		void VerifyReturnValueLocation()
 		{
-			if (returnValueLocation.has_value() && returnValueLocation == Location::STACK)
+			if (returnValueLocation.has_value() && returnValueLocation == Location::Stack)
 				throw std::invalid_argument("Return value location can not be stack");
-
 		}
 
 		void VerifyArgumentLocations()
@@ -177,7 +217,15 @@ namespace Unconventional
 	class Function
 	{
 	public:
+		Function() : address(0x0), signature(FunctionSignature<ReturnType, ArgumentTypes...>())
+		{
+		}
+
 		Function(const uintptr_t address, const FunctionSignature<ReturnType, ArgumentTypes...> signature) : address(address), signature(signature) {}
+
+		uintptr_t GetAddress() const { return address; }
+
+		FunctionSignature<ReturnType, ArgumentTypes...> GetSignature() { return signature; };
 
 		// TODO: This does not support doubles currently
 		ReturnType Call(ArgumentTypes... arguments)
@@ -220,7 +268,7 @@ namespace Unconventional
 			uint32_t stackArgumentCount = stackArgumentIndices.size();
 			auto byteSizeOfStackArguments = stackArgumentCount * 4;
 
-			auto argumentCleanupByteSize = signature.ConventionSpecifiesCallerCleanup() ? byteSizeOfStackArguments : 0;
+			auto argumentCleanupByteSize = signature.GetCallingConvention().SpecifiesCallerCleanup() ? byteSizeOfStackArguments : 0;
 
 			auto stackArguments = (uint8_t*)malloc(byteSizeOfStackArguments);
 			uint32_t stackArgumentsWriteIndex = 0;
@@ -321,25 +369,25 @@ namespace Unconventional
 				return *(ReturnType*)&ediValue;
 
 			case Location::AL:
-				return (ReturnType)GetLowByte(eaxValue);
+				return (ReturnType)Utils::GetLowByte(eaxValue);
 			case Location::AH:
-				return (ReturnType)GetHighByte(eaxValue);
+				return (ReturnType)Utils::GetHighByte(eaxValue);
 			case Location::BL:
-				return (ReturnType)GetLowByte(ebxValue);
+				return (ReturnType)Utils::GetLowByte(ebxValue);
 			case Location::BH:
-				return (ReturnType)GetHighByte(ebxValue);
+				return (ReturnType)Utils::GetHighByte(ebxValue);
 			case Location::CL:
-				return (ReturnType)GetLowByte(ecxValue);
+				return (ReturnType)Utils::GetLowByte(ecxValue);
 			case Location::CH:
-				return (ReturnType)GetHighByte(ecxValue);
+				return (ReturnType)Utils::GetHighByte(ecxValue);
 			case Location::DL:
-				return (ReturnType)GetLowByte(edxValue);
+				return (ReturnType)Utils::GetLowByte(edxValue);
 			case Location::DH:
-				return (ReturnType)GetHighByte(edxValue);
+				return (ReturnType)Utils::GetHighByte(edxValue);
 			case Location::SIL:
-				return (ReturnType)GetLowByte(esiValue);
+				return (ReturnType)Utils::GetLowByte(esiValue);
 			case Location::DIL:
-				return (ReturnType)GetLowByte(ediValue);
+				return (ReturnType)Utils::GetLowByte(ediValue);
 
 			case Location::ST0:
 				return (ReturnType)st0Value;
@@ -358,7 +406,7 @@ namespace Unconventional
 			case Location::ST7:
 				return (ReturnType)st7Value;
 
-			case Location::STACK:
+			case Location::Stack:
 				throw std::exception("Return value cannot be on stack");
 			default:
 				throw std::exception("Not implemented");
@@ -367,15 +415,301 @@ namespace Unconventional
 	private:
 		uintptr_t address;
 		FunctionSignature<ReturnType, ArgumentTypes...> signature;
-
-		static uint8_t GetLowByte(uint32_t x)
+	};
+	
+	template<typename ReturnType, typename... ArgumentTypes>
+	class Hook
+	{
+	public:
+		void Install()
 		{
-			return x & 0xFF;
+			if (!isInitialized)
+			{
+				throw std::logic_error("Hook was not initialized");
+			}
+
+			if (!isInstalled) 
+			{
+				WriteJump(originalFunction.GetAddress(), hookWrapperAddress);
+				isInstalled = true;
+			}
 		}
 
-		static uint8_t GetHighByte(uint32_t x)
+		void Uninstall()
 		{
-			return (x >> 8) & 0xFF;
+			if (!isInitialized)
+			{
+				throw std::logic_error("Hook was not initialized");
+			}
+
+			if (isInstalled) 
+			{
+				std::memcpy((void*)originalFunction.GetAddress(), (void*)trampolineAddress, opCodeSize);
+				isInstalled = false;
+			}
 		}
+
+		ReturnType CallOriginalFunction(ArgumentTypes... arguments)
+		{
+			Function<ReturnType, ArgumentTypes...> trampolineFunction(trampolineAddress, originalFunction.GetSignature());
+			return trampolineFunction.Call(arguments...);
+		}
+
+		Hook() : isInitialized(false), isInstalled(false), opCodeSize(0), originalFunction(Function<ReturnType, ArgumentTypes...>()), userHookFunctionAddress(0),
+		         trampolineAddress(0),
+		         hookWrapperAddress(0),
+		         tempStorage{}
+		{
+		}
+
+		Hook(Function<ReturnType, ArgumentTypes...> originalFunction, uintptr_t hookFunctionAddress, const uint8_t opCodeSize)
+			: isInstalled(false), opCodeSize(opCodeSize), originalFunction(originalFunction), userHookFunctionAddress(hookFunctionAddress), trampolineAddress(0), hookWrapperAddress(0)
+		{
+			if (opCodeSize < 5)
+			{
+				throw std::invalid_argument("At least 5 bytes are required for hooking");
+			}
+
+			SetupTrampoline();
+			SetupHookWrapper();
+
+			isInitialized = true;
+		}
+
+		~Hook()
+		{
+			if (isInitialized) 
+			{
+				Uninstall();
+
+				VirtualFree((void*)trampolineAddress, trampolineSize, MEM_RELEASE);
+				VirtualFree((void*)hookWrapperAddress, MAX_HOOK_WRAPPER_CODE_SIZE, MEM_RELEASE);
+			}
+		}
+
+	private:
+		bool isInitialized;
+		bool isInstalled;
+
+		uint8_t opCodeSize;
+		Function<ReturnType, ArgumentTypes...> originalFunction;
+		uintptr_t userHookFunctionAddress;
+
+		uintptr_t trampolineAddress;
+		uint32_t trampolineSize;
+
+		uintptr_t hookWrapperAddress;
+		std::uint32_t tempStorage[128];
+
+		static constexpr uint32_t MAX_HOOK_WRAPPER_CODE_SIZE = 512;
+		static constexpr uint8_t SIZE_OF_JUMP = 5;
+
+		static void WriteJump(const std::uintptr_t address, const std::uintptr_t target)
+		{
+			DWORD oldProtection;
+			VirtualProtect((void*)address, SIZE_OF_JUMP, PAGE_EXECUTE_READWRITE, &oldProtection);
+
+			const auto relativeJumpOffset = target - address - SIZE_OF_JUMP;
+
+			*(uint8_t*)address = 0xE9;
+			*(uint32_t*)(address + 1) = relativeJumpOffset;
+		}
+
+		void SetupTrampoline()
+		{
+			trampolineSize = opCodeSize + SIZE_OF_JUMP;
+
+			trampolineAddress = (uintptr_t)VirtualAlloc(nullptr, trampolineSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+			std::memcpy((void*)trampolineAddress, (void*)originalFunction.GetAddress(), opCodeSize);
+
+			WriteJump(trampolineAddress + opCodeSize, originalFunction.GetAddress() + opCodeSize);
+		}
+
+		void SetupHookWrapper()
+		{
+			hookWrapperAddress = (uintptr_t)VirtualAlloc(nullptr, MAX_HOOK_WRAPPER_CODE_SIZE, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+
+			std::vector<uint8_t> hookWrapperBytes;
+
+			// Pop the return address
+			{
+				const auto address = (uintptr_t)&tempStorage[0];
+				hookWrapperBytes.push_back(0x8F);
+				hookWrapperBytes.push_back(0x05);
+				hookWrapperBytes.push_back(Utils::GetLowByte(address));
+				hookWrapperBytes.push_back(Utils::GetHighByte(address));
+				hookWrapperBytes.push_back(Utils::GetLowByte(address >> 16));
+				hookWrapperBytes.push_back(Utils::GetHighByte(address >> 16));
+			}
+
+			const uint32_t stackArgumentCount = originalFunction.GetSignature().GetStackArgumentIndices().size();
+			for (uint32_t i = 0; i < stackArgumentCount; i++)
+			{
+				// Write pop to local storage location
+				const auto address = (uintptr_t)&tempStorage[i + 1];
+				hookWrapperBytes.push_back(0x8F);
+				hookWrapperBytes.push_back(0x05);
+				hookWrapperBytes.push_back(Utils::GetLowByte(address));
+				hookWrapperBytes.push_back(Utils::GetHighByte(address));
+				hookWrapperBytes.push_back(Utils::GetLowByte(address >> 16));
+				hookWrapperBytes.push_back(Utils::GetHighByte(address >> 16));
+			}
+
+
+			// Push all registers
+			hookWrapperBytes.push_back(0x60);
+
+			// Write a push for each argument
+			auto argumentLocations = originalFunction.GetSignature().GetArgumentLocations();
+			std::reverse(argumentLocations.begin(), argumentLocations.end());
+			uint32_t stackWriteIndex = 0;
+			for (const Location location : argumentLocations)
+			{
+				switch (location)
+				{
+				case Location::Stack:
+				{
+					const auto address = (uintptr_t)&tempStorage[stackArgumentCount - stackWriteIndex++];
+					hookWrapperBytes.push_back(0xFF);
+					hookWrapperBytes.push_back(0x35);
+					hookWrapperBytes.push_back(Utils::GetLowByte(address));
+					hookWrapperBytes.push_back(Utils::GetHighByte(address));
+					hookWrapperBytes.push_back(Utils::GetLowByte(address >> 16));
+					hookWrapperBytes.push_back(Utils::GetHighByte(address >> 16));
+				}
+					break;
+				case Location::EAX:
+					hookWrapperBytes.push_back(0x50);
+					break;
+				case Location::EBX:
+					hookWrapperBytes.push_back(0x53);
+					break;
+				case Location::ECX:
+					hookWrapperBytes.push_back(0x51);
+					break;
+				case Location::EDX:
+					hookWrapperBytes.push_back(0x52);
+					break;
+				case Location::ESI:
+					hookWrapperBytes.push_back(0x56);
+					break;
+				case Location::EDI:
+					hookWrapperBytes.push_back(0x57);
+					break;
+				default:
+					throw std::exception("Not yet implemented");
+				}
+			}
+
+			// Write call
+			hookWrapperBytes.push_back(0xE8);
+			const auto relativeCallOffset = userHookFunctionAddress - (hookWrapperAddress + hookWrapperBytes.size() - 1) - 5;
+			hookWrapperBytes.push_back(Utils::GetLowByte(relativeCallOffset));
+			hookWrapperBytes.push_back(Utils::GetHighByte(relativeCallOffset));
+			hookWrapperBytes.push_back(Utils::GetLowByte(relativeCallOffset >> 16));
+			hookWrapperBytes.push_back(Utils::GetHighByte(relativeCallOffset >> 16));
+
+			// add esp, X
+			hookWrapperBytes.push_back(0x83);
+			hookWrapperBytes.push_back(0xC4);
+			hookWrapperBytes.push_back((uint8_t)argumentLocations.size() * 4);
+
+			// Save EAX to tempStorage
+			const auto eaxStorageAddress = (uintptr_t)&tempStorage[1];
+			hookWrapperBytes.push_back(0xA3);
+			hookWrapperBytes.push_back(Utils::GetLowByte(eaxStorageAddress));
+			hookWrapperBytes.push_back(Utils::GetHighByte(eaxStorageAddress));
+			hookWrapperBytes.push_back(Utils::GetLowByte(eaxStorageAddress >> 16));
+			hookWrapperBytes.push_back(Utils::GetHighByte(eaxStorageAddress >> 16));
+			
+
+			// Pop all registers
+			hookWrapperBytes.push_back(0x61);
+
+			// Put return value where it needs to go
+			// We sort of assume the user's hook function itself to be CDECL
+			auto returnValueLocation = CallingConvention(CallingConvention::Convention::Cdecl).GetReturnValueLocation(std::is_floating_point<ReturnType>());
+			if (returnValueLocation == Location::EAX)
+			{
+				switch (originalFunction.GetSignature().GetReturnValueLocation())
+				{
+				case Location::EAX:
+					hookWrapperBytes.push_back(0xA1);
+					break;
+				case Location::EBX:
+					hookWrapperBytes.push_back(0x8B);
+					hookWrapperBytes.push_back(0x1D);
+					break;
+				case Location::ECX:
+					hookWrapperBytes.push_back(0x8B);
+					hookWrapperBytes.push_back(0x0D);
+					break;
+				case Location::EDX:
+					hookWrapperBytes.push_back(0x8B);
+					hookWrapperBytes.push_back(0x15);
+					break;
+				case Location::ESI:
+					hookWrapperBytes.push_back(0x8B);
+					hookWrapperBytes.push_back(0x35);
+					break;
+				case Location::EDI:
+					hookWrapperBytes.push_back(0x8B);
+					hookWrapperBytes.push_back(0x3D);
+					break;
+				// TODO: Handle 8-bit registers
+				default:
+					throw std::exception("Return value location not implemented");
+				}
+				hookWrapperBytes.push_back(Utils::GetLowByte(eaxStorageAddress));
+				hookWrapperBytes.push_back(Utils::GetHighByte(eaxStorageAddress));
+				hookWrapperBytes.push_back(Utils::GetLowByte(eaxStorageAddress >> 16));
+				hookWrapperBytes.push_back(Utils::GetHighByte(eaxStorageAddress >> 16));
+			}
+			else if (returnValueLocation == Location::ST0)
+			{
+				// Pop all registers
+				hookWrapperBytes.push_back(0x61);
+
+				switch (originalFunction.GetSignature().GetReturnValueLocation())
+				{
+				case Location::ST0:
+					break;
+				// TODO: Implement these on-demand
+				default:
+					throw std::exception("Floating-point return value location not implemented");
+				}
+			}
+			else
+			{
+				throw std::logic_error("Return value location for CDECL function was neither EAX nor ST0");
+			}
+
+			// sub esp, X
+			hookWrapperBytes.push_back(0x83);
+			hookWrapperBytes.push_back(0xEC);
+			hookWrapperBytes.push_back((uint8_t)stackArgumentCount * 4);
+
+			// Push return address to top of stack again
+			{
+				const auto address = (uintptr_t)&tempStorage[0];
+				hookWrapperBytes.push_back(0xFF);
+				hookWrapperBytes.push_back(0x35);
+				hookWrapperBytes.push_back(Utils::GetLowByte(address));
+				hookWrapperBytes.push_back(Utils::GetHighByte(address));
+				hookWrapperBytes.push_back(Utils::GetLowByte(address >> 16));
+				hookWrapperBytes.push_back(Utils::GetHighByte(address >> 16));
+			}
+
+			// Write Return
+			hookWrapperBytes.push_back(0xC3);
+
+			// Write hook wrapper to memory
+			if (hookWrapperBytes.size() > MAX_HOOK_WRAPPER_CODE_SIZE)
+				throw std::logic_error("Hook Wrapper Function byte size was larger than MAX_HOOK_WRAPPER_CODE_SIZE");
+
+			std::memcpy((void*)hookWrapperAddress, hookWrapperBytes.data(), hookWrapperBytes.size());
+		}
+		
 	};
 }
